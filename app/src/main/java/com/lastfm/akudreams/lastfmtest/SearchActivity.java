d577dfb46bbf3276f3f7f60b4e43ec654464c1f5
Jorge Perez Ruiz <akumayken@gmail.com>
2017-08-29 12:13:33 +0200

    Add Search Bar to search for albums

--- app/src/main/java/com/lastfm/akudreams/lastfmtest/SearchActivity.java
+++ app/src/main/java/com/lastfm/akudreams/lastfmtest/SearchActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.arlib.floatingsearchview.FloatingSearchView;
+import com.arlib.floatingsearchview.suggestions.model.SearchSuggestion;
 import com.lastfm.akudreams.lastfmtest.models.Album;
 
 import java.util.List;
@@ -15,8 +17,22 @@ public class SearchActivity extends AppCompatActivity implements SearchAlbumList
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
 
-        new NetworkService().searchAlbums("space", this);
+        FloatingSearchView searchView = findViewById(R.id.floating_search_view);
+        searchView.setOnSearchListener(onSearchListener);
+
     }
+
+    private final FloatingSearchView.OnSearchListener onSearchListener = new FloatingSearchView.OnSearchListener() {
+        @Override
+        public void onSuggestionClicked(SearchSuggestion searchSuggestion) {
+            // no op
+        }
+
+        @Override
+        public void onSearchAction(String currentQuery) {
+            new NetworkService().searchAlbums(currentQuery, SearchActivity.this);
+        }
+    };
 
     @Override
     public void onAlbumsLoaded(List<Album> albums) {
